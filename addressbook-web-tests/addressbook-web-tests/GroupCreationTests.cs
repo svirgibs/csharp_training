82cc9ffe2b6de32273d91bf1b2303c309ae1a6b5
Denis Yavorskiy <svirgibs@gmail.com>
2018-09-29 12:22:03 +0300

    Лекция 1.6. Передача параметров в методы, шаблон проектирования Value Object

--- addressbook-web-tests/addressbook-web-tests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/GroupCreationTests.cs
@@ -47,10 +47,13 @@ namespace WebAddressbookTests
         public void GroupCreationTest()
         {
             OpenHomePage();
-            Login("admin", "secret");
+            Login(new AccountData("admin", "secret"));
             GoToGroupsPage();
             InitGroupCreation();
-            FillGroupForm("aaa", "sss", "ddd");
+            GroupData group = new GroupData("aaa");
+            group.Header = "ddd";
+            group.Footer = "fff";
+            FillGroupForm(group);
             SubmitGroupCreation();
             ReturnToGroupPage();
             Logout();
@@ -71,15 +74,15 @@ namespace WebAddressbookTests
             driver.FindElement(By.Name("submit")).Click();
         }
 
-        private void FillGroupForm(string name, string header, string footer)
+        private void FillGroupForm(GroupData group)
         {
             driver.FindElement(By.Name("group_name")).Click();
             driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys(name);
+            driver.FindElement(By.Name("group_name")).SendKeys(group.Name);
             driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys(header);
+            driver.FindElement(By.Name("group_header")).SendKeys(group.Header);
             driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys(footer);
+            driver.FindElement(By.Name("group_footer")).SendKeys(group.Footer);
         }
 
         private void InitGroupCreation()
@@ -92,14 +95,14 @@ namespace WebAddressbookTests
             driver.FindElement(By.LinkText("groups")).Click();
         }
 
-        private void Login(string username, string password)
+        private void Login(AccountData account)
         {
             driver.FindElement(By.Name("user")).Click();
             driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys(username);
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
             driver.FindElement(By.Name("pass")).Click();
             driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys(password);
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
             driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
         }
 
--- addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
+++ addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
@@ -7,11 +7,38 @@ namespace addressbook_web_tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodSquare()
         {
-            Square s1 = new Square();
-            Square s2 = new Square();
+            Square s1 = new Square(5);
+            Square s2 = new Square(10);
             Square s3 = s1;
+
+            Assert.AreEqual(s1.Size, 5);
+            Assert.AreEqual(s2.Size, 10);
+            Assert.AreEqual(s3.Size, 5);
+
+            s3.Size = 15;
+            Assert.AreEqual(s1.Size, 15);
+
+            s2.Colored = true;
+
+        }
+
+        [TestMethod]
+        public void TestMethodCircle()
+        {
+            Circle s1 = new Circle(5);
+            Circle s2 = new Circle(10);
+            Circle s3 = s1;
+
+            Assert.AreEqual(s1.Radius, 5);
+            Assert.AreEqual(s2.Radius, 10);
+            Assert.AreEqual(s3.Radius, 5);
+
+            s3.Radius = 15;
+            Assert.AreEqual(s1.Radius, 15);
+
+
         }
     }
 }
